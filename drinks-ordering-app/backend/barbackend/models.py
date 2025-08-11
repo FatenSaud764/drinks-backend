f6104c42cf63bf84a59d81d704f4ea27c718472e
Jon-Patrick Booysen <27129500@sun.ac.za>
2025-08-11 18:12:53 +0200

    Add note to orders

--- drinks-ordering-app/backend/barbackend/models.py
+++ drinks-ordering-app/backend/barbackend/models.py
@@ -59,6 +59,7 @@ class Order(models.Model):
     ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    note = models.TextField(blank=True, default="")
     status = models.CharField(max_length=30, choices=STATUS_CHOICES, default='pending')
     total_price = models.DecimalField(max_digits=8, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
