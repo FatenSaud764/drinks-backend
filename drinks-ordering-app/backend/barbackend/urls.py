99a7109bd48bd4d25c0f28c6f960eed7609393e2
Jon-Patrick Booysen <27129500@sun.ac.za>
2025-09-10 13:52:41 +0200

    Modify URL name

--- drinks-ordering-app/backend/barbackend/urls.py
+++ drinks-ordering-app/backend/barbackend/urls.py
@@ -31,7 +31,7 @@ router.register('drink', DrinkViewset, basename='drink')
 router.register('orders', OrderViewset, basename='orders')
 router.register('cart', CartViewset, basename='cart')
 router.register('auth', AuthViewset, basename='auth')
-router.register('staff-users', StaffUserViewset, basename='staff-users')
+router.register('management', StaffUserViewset, basename='management')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
