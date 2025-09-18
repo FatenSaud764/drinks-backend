bb33866022d91463950f75185acd7bf211bfaf24
Faten Saud <26950898@sun.ac.za>
2025-09-18 13:27:40 +0200

    Fixed incorrect loading import

--- drinks-ordering-app/client/src/App.jsx
+++ drinks-ordering-app/client/src/App.jsx
@@ -97,7 +97,7 @@ const AppContent = () => {
                   <UserCart.Provider value={{cart, setCart}}>
                     <SignUpModal.Provider value={{signupmodal, setSignUpModal}}>
                       <Routes>
-                        <Route path='/' element={isLoading ? <div></div> : !isLoggedIn || !accessToken || accessToken === 'null' ? <Login /> : <Navigate to="/products" />} />
+                        <Route path='/' element={!isLoggedIn || !accessToken || accessToken === 'null' ? <Login /> : <Navigate to="/products" />} />
                         <Route path='/products' element={<Products />} />
                         <Route path='/cart' element={
                           <ProtectedRoute>
