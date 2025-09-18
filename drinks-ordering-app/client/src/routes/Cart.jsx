c7aecf4f51f6b3363495606da433a55c4e03beb5
Faten Saud <26950898@sun.ac.za>
2025-09-18 22:14:50 +0200

    Checkout snackbar

--- drinks-ordering-app/client/src/routes/Cart.jsx
+++ drinks-ordering-app/client/src/routes/Cart.jsx
@@ -6,12 +6,17 @@ import './Cart.css'
 import { useState } from 'react'
 import AxiosInstance from '../components/Axios'
 import { FaRegTrashCan } from "react-icons/fa6";
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert'
+import Slide from '@mui/material/Slide'
+
 
 const Cart = () => {
   const { theme, setTheme } = useContext(LightDark)
   const { products, setProducts } = useContext(ProductList)
   const { cart, setCart } = useContext(UserCart);
   const [cartItems, setCartItems] = useState([]);
+  const [open, setOpen] = useState(false);
 
   // Use the new auth context
   const { accessToken, isLoggedIn } = useAuth();
@@ -28,7 +33,6 @@ const Cart = () => {
         },
       });
       setCartItems(res.data.items);
-      console.log('my cart', cartItems);
     } catch (err) {
       console.error(err);
     }
@@ -42,12 +46,14 @@ const Cart = () => {
         headers: { Authorization: `Bearer ${accessToken}` }
       })
       console.log('order', result.data)
-      alert("Placed order!")
+      setOpen(true);
       fetchdata()
     } catch (err) {
       console.error(err);
     }
   }
+
+  console.log('open', open)
 
   const ClearCart = async() => {
     if (!isLoggedIn || !accessToken) return;
@@ -123,6 +129,16 @@ const Cart = () => {
     setCartItems(cartItems.filter(item => item.id !== id));
   };
 
+  useEffect(() => {
+  if (open) {
+    const timer = setTimeout(() => {
+      setOpen(false);
+    }, 3000);
+    
+    return () => clearTimeout(timer); // Cleanup
+  }
+}, [open]);
+
   const totalPrice = cartItems.reduce((sum, item) => sum + products.filter(product => { return product.id === item.drink_id }).map(product => product.price) * item.quantity, 0);
 
   // Show login message if not authenticated
@@ -140,6 +156,28 @@ const Cart = () => {
   return (
     <div className="cartwrapper" id={theme}>
       <NavBar />
+      <Slide in={open} direction='right'>
+        <Snackbar open={open} onClose={() => {setOpen(false)}} anchorOrigin={{vertical: 'bottom', horizontal: 'left'}} sx={{bottom: '2vh', left: '2vh'}}><Alert onClose={() => {setOpen(false)}} severity="success" sx={
+          {
+            display: 'flex',
+            alignItems: 'center',
+            width: '70vw',
+            background: '#2563EB',
+            color: 'white',
+            '& .MuiAlert-icon': {
+              alignItems: 'center',
+      color: 'white',
+    },
+    '& .MuiAlert-message': {
+      alignItems: 'center',
+      fontWeight: '800',
+      fontFamily: 'Roboto Slab',
+      fontSize: '18px',
+      color: 'white',
+    }
+          }
+        }>Order placed!</Alert></Snackbar>
+      </Slide>
       <div className='cart-items'>
         <div className="cart-items-list">
           {cartItems.length === 0 ? (
