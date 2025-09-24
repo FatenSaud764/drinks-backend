3b6becbe75fc91028baeebceb15389a450d3c06a
Kirsten Sanders <27211266@sun.ac.za>
2025-09-24 12:34:10 +0200

    updated backend

--- drinks-ordering-app/backend/barbackend/management/commands/create_dummy_data.py
+++ drinks-ordering-app/backend/barbackend/management/commands/create_dummy_data.py
@@ -94,7 +94,7 @@ class Command(BaseCommand):
             ("Espresso Martini", "EspressoMartini.jpg", Decimal('95.00'), "cocktail", 50),
             ("Gin & Tonic", "GinTonic.jpg", Decimal('90.00'), "cocktail", 40),
             ("Mimosa", "Mimosa.png", Decimal('80.00'), "cocktail", 45),
-            ("Passionfruit Martini", "PassionMartini.jpg", Decimal('90.50'), "cocktail", 30),
+            ("Passionfruit Martini", "PassionMartini.png", Decimal('90.50'), "cocktail", 30),
             ("Peach Vodka", "PeachVodka.jpg", Decimal('72.00'), "cocktail", 25),
             ("Tequila Shot", "TequilaSour.png", Decimal('35.00'), "cocktail", 60),
             ("Pina Colada", "PinaColada.jpg", Decimal('85.00'), "cocktail", 20),
@@ -110,7 +110,6 @@ class Command(BaseCommand):
             # Shots
             ("Jagermeister", "Jagermeister.jpg", Decimal('50.00'), "shot", 70),
             ("Whiskey Shot", "WhiskeyShot.jpg", Decimal('55.00'), "shot", 65),
-            ("Vodka Shot", "VodkaShot.jpg", Decimal('45.00'), "shot", 80),
             ]
 
         def resolve_image(filename: str) -> str:
