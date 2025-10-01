d09b0965086cda3591f9e2e541e6fbfd2a4d223d
Faten Saud <26950898@sun.ac.za>
2025-10-01 13:51:57 +0200

    Changed api timeout admin side

--- drinks-ordering-app/admin/src/api/api.js
+++ drinks-ordering-app/admin/src/api/api.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8000/",
-  timeout: 10000,
+  timeout: 50000,
   headers: {
     accept: "application/json"
   },
