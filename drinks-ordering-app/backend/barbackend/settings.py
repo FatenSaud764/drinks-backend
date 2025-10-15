3eac91fc46dd16401addedebb3257ecb38771675
Faten Saud <26950898@sun.ac.za>
2025-10-15 02:02:16 +0200

    Switched from Gmail to SendGrid

--- drinks-ordering-app/backend/barbackend/settings.py
+++ drinks-ordering-app/backend/barbackend/settings.py
@@ -190,10 +190,6 @@ DEFAULT_FILE_STORAGE = "barbackend.storage_backends.SupabaseStorage"
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
-    EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-    EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-    EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+    EMAIL_BACKEND = 'sendgrid_backend.SendGridBackend'
     DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='fatensaud04@gmail.com')
+    SENDGRID_API_KEY = config('SENDGRID API KEY', default = '')
